7f1edca679b8ecb7354bdf6c4f3b38e332eccfc8
LuanNDHE180066 <luanndhe180066@fpt.edu.vn>
2025-03-08 17:33:56 +0700

    ok

--- FinalProject/obj/Debug/net8.0-windows/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0-windows/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4708906f7abafe8955fbc005b4c19915ecea8246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2632d8266029d4c25445e4fe5914c3f0e367b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
