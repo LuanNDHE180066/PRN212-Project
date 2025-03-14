44bf6c3371dca37903f29769fd4eb17e66f502ef
quangpnhe180573 <quangdpcl@gmail.com>
2025-03-14 18:45:17 +0700

    Update, Add, Good

--- FinalProject/obj/Debug/net8.0-windows/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0-windows/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18964ee59d7aae2e598fffbbac57bf9fafc40f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82fd95bee444554f11c7117bc940bc4b7ef014eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
