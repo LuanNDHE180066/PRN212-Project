df946fee04b8291a20adf98edf706c7a5fb36922
quangpnhe180573 <quangdpcl@gmail.com>
2025-03-25 20:29:56 +0700

    change app.xaml

--- FinalProject/obj/Debug/net8.0-windows/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0-windows/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae93b5f3539eb319171d5d0c3eae92847062431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a3121c4d2f59a9b5985fb60467e7789146a888")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
